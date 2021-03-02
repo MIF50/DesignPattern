a7c9ad43c1aa5a903af4d9e571aa39ba002239b3
mohamed ibrahim <ibrahim@beinmedia.com>
2021-03-02 09:38:54 +0300

    finish decorator design pattern

---
+++ src/com/MIF50/decorator/Stream.java
@@ -0,0 +1,5 @@
+package com.MIF50.decorator;
+
+public interface Stream {
+    void write(String data);
+}
---
+++ src/com/MIF50/decorator/exercise/AbstractArtefact.java
@@ -0,0 +1,5 @@
+package com.MIF50.decorator.exercise;
+
+public interface AbstractArtefact {
+    String render();
+}
